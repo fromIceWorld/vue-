884eb49a483c6de08a2aefe7738785c50b130368
Kraken <752856108@qq.com>
2020-03-26 13:17:46 +0800

    feat:模板解析

--- moni.js
+++ moni.js
@@ -54,7 +54,7 @@ function type(data,prop,val){
 let startTagStart = /^\s*<([a-zA-Z_]+)/     //开始标签的开始
 let startTagEnd = /^\s*>/     //开始标签的结束
 let attribute = /^\s*([^\s"'<>\/=]+)(?:\s*(=)\s*(?:"([^"]*)"+|'([^']*)'+|([^\s"'=<>`]+)))?/   //匹配属性
-let checkText = /^\s*(\w*)(\s*\{\{\w*\}\})(\s*\w*)/
+let checkText = /^\s*(\w*)(\s*\{\{\w*\}\})*(\s*\w*)*/
 let staticText = /^\s*\w+/      //静态文字
 let vueData = /^\s*\{\{([a-zA-Z_]+)\}\}/   // 动态数据{{data}}
 let endTag = /^\s*<\/(\w+)>/
@@ -62,51 +62,90 @@ let stack = []
 let html = '<div id="app" class="class">{{msg}}<span>Vue</span></div>'
 
   //开始标签
-function parseStartTag(html){
+function parseStartTag(){
   if(html.match(startTagStart)){
     const start = html.match(startTagStart)
     const match ={
       tagName:start[1],
       attrs:[]
     }
-    let tag = html.substring(start[0].length)
+    html = html.substring(start[0].length)
     let end , attr 
-    while(!(end = tag.match(startTagEnd))){
-      attr = tag.match(attribute)
+    while(!(end = html.match(startTagEnd))){
+      attr = html.match(attribute)
       match.attrs.push(attr)
-      tag = tag.substring(attr[0].length)
+      html = html.substring(attr[0].length)
     }
     return match
   }
 }
   //文本
-function parseText(text){
-  if(text.match(checkText)){
-    const textData = text.match(checkText)
+function parseText(){
     let end , 
         token = [] 
-    while(!(end = text.match(startTagStart))){
-      if(text.match(staticText)){
-        let stText = text.match(staticText)
+    while(html.match(vueData) || html.match(staticText)){
+      if(html.match(staticText)){
+        let stText = html.match(staticText)
           token.push(stText[0])
-          text = text.substring(stText[0].length)
-      }else if(text.match(vueData)){
-        let stText = text.match(vueData)
+          html = html.substring(stText[0].length)
+      }else if(html.match(vueData)){
+        let stText = html.match(vueData)
         token.push(`_s(${stText[1].trim()})`)        //动态数据 _s函数进行操作
-        text = text.substring(stText[0].length)
+        html = html.substring(stText[0].length)
       }
     }
     return token
-  }
+}
+//开始标签的结束
+function parseEndTag(){
+    let mid = html.match(startTagEnd)
+    html = html.substring(mid[0].length)
 }
 //结束标签
-function parseEndTag(html){
-  if(html.match(endTag)){
-
-  }
+function parseEnd(){
+    let mid = html.match(endTag)
+    html = html.substring(mid[0].length)
 }
 
 //解析html
-function parseHtml(html){
-   
+function parseHtml(){
+  let stack = [],currentParent,root
+   //开始标签
+   while(html.length!== 0){
+     //开始标签
+    if(html.match(startTagStart)){
+      let mid = parseStartTag()
+      let element = {
+        type:1,
+        tag:mid.tagName,
+        lowerCasedTag:mid.tagName.toLowerCase(),
+        attrsMap:'makeAttrsMap(mid.attrs)',
+        children:[]
+      }
+      if(!root){
+        root = element
+      }
+      if(currentParent){
+        currentParent.children.push(element)
+      }
+      stack.push(element)
+      currentParent = element
+    }
+    // 开始标签的闭合
+    if(html.match(startTagEnd)){
+      parseEndTag()
+    }
+    //文本
+    if(html.match(vueData) || html.match(staticText)){
+      let mid = this.parseText()
+      currentParent.children.push({
+        type:2,
+        text:mid
+      })
+    }
+    if(html.match(endTag)){
+      parseEnd()
+    }
+} 
+console.log(stack,currentParent,root)
 }
